eaebd6029e4b1b5a189a2d7b0329f3bd9253df6c
Salvatore Sferrazza <sal@pangea.io>
2025-11-07 07:11:58 -0500

    parameterize # of peer points-of-sale

--- example/runpos.py
+++ example/runpos.py
@@ -6,19 +6,19 @@ from pos import PointOfSale
 from concurrent.futures import ThreadPoolExecutor
 from threading import Thread
 
+NUM_POS = 5
+WORKERS = NUM_POS * 2
+
 def main():
 
-    pos1 = PointOfSale(sys.argv[1], sys.argv[2], remote_port=int(sys.argv[3]))
-    pos2 = PointOfSale(sys.argv[1], sys.argv[2], remote_port=int(sys.argv[3]))
-    pos3 = PointOfSale(sys.argv[1], sys.argv[2], remote_port=int(sys.argv[3]))
+    posexec = ThreadPoolExecutor(max_workers=WORKERS)
 
-    with ThreadPoolExecutor(max_workers=6) as posexec:
-        posexec.submit(pos1.generate_orders)
-        posexec.submit(pos2.generate_orders)
-        posexec.submit(pos3.generate_orders)
-        posexec.submit(pos1.listen)
-        posexec.submit(pos2.listen)                        
-        posexec.submit(pos3.listen)
+    i = 0
+    while i < NUM_POS:
+        pos = PointOfSale(sys.argv[1], sys.argv[2], remote_port=int(sys.argv[3]))
+        posexec.submit(pos.generate_orders)
+        posexec.submit(pos.listen)
+        i += 1
 
         
 if __name__ == "__main__":
