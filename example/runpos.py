b86ca1c10f4634db69335fe5b01cb90d528d9a56
Salvatore Sferrazza <sal@pangea.io>
2025-11-06 22:46:28 -0500

    identifier name

--- example/runpos.py
+++ example/runpos.py
@@ -8,17 +8,17 @@ from threading import Thread
 
 def main():
 
-    pos1 = PointOfSale(sys.argv[1], sys.argv[2], remote_port=3001)
-    pos2 = PointOfSale(sys.argv[1], sys.argv[2], remote_port=3001)
-    pos3 = PointOfSale(sys.argv[1], sys.argv[2], remote_port=3001)
+    pos1 = PointOfSale(sys.argv[1], sys.argv[2], remote_port=int(sys.argv[3])
+    pos2 = PointOfSale(sys.argv[1], sys.argv[2], remote_port=int(sys.argv[3])
+    pos3 = PointOfSale(sys.argv[1], sys.argv[2], remote_port=int(sys.argv[3])
 
-    with ThreadPoolExecutor(max_workers=6) as whsexec:
-        whsexec.submit(pos1.generate_orders)
-        whsexec.submit(pos2.generate_orders)
-        whsexec.submit(pos3.generate_orders)
-        whsexec.submit(pos1.listen)
-        whsexec.submit(pos2.listen)                        
-        whsexec.submit(pos3.listen)
+    with ThreadPoolExecutor(max_workers=6) as posexec:
+        posexec.submit(pos1.generate_orders)
+        posexec.submit(pos2.generate_orders)
+        posexec.submit(pos3.generate_orders)
+        posexec.submit(pos1.listen)
+        posexec.submit(pos2.listen)                        
+        posexec.submit(pos3.listen)
 
         
 if __name__ == "__main__":
