c604432c68cf9d15d18e1b6afedb65db048c207f
Salvatore Sferrazza <sal@pangea.io>
2025-11-16 11:58:34 -0500

    broadcast inventory upon warehouse boot

--- example/warehouse.py
+++ example/warehouse.py
@@ -1,14 +1,35 @@
+import sys
+
 from inventory import InventoryDestination
 from sender import SenderMixin
+from time import sleep
 
 class Warehouse(InventoryDestination, SenderMixin):
     def __init__(self, group, port, remote_port):
+        self.file_path = "data/inventory.txt"
         print("warehouse init")
         self.remote_port = remote_port
         self.connect("localhost", remote_port)
         print("warehouse init done")
         super().__init__(group, port)
+        sleep(5)
+        print("submitting inventory")
+        self.submit_inventory()
         
+
+    def submit_inventory(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                for line in file:
+                    self.send(line.strip())
+        except FileNotFoundError:
+            print(f"Error: The file '{file_path}' was not found.")
+            sys.exit(1)
+            
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            sys.exit(1)
+            
     def on_message(self, seq, msg):
         super().on_message(seq, msg)
         msg_fields = msg.split()
@@ -33,12 +54,12 @@ class Warehouse(InventoryDestination, SenderMixin):
         # Only send the network a decrement
         # if there exists sufficient inventory
         if current_level is not None:
-            print(f"#{self.last_sequence_number} {sku} wants: {qty}, has: {current_level}, enough? {current_level >= qty}")
+            print(f"#{self.last_sequence_number} wants {qty} of {sku}, {current_level} are in stock")
             if current_level >= qty:
                 self.inventory.apply(sku, qty * -1) # obviates call to on_inventory_delta()
                 self.send(" ".join(["D", str(qty * -1), sku]))
             else:
-                print(f"Insufficient inventory to fulflll {qty} of {sku}: {current_level}")
+                print(f"Insufficient inventory of {current_level} to fulflll {qty} of {sku}")
         else:
             print(f"unknown sku: {sku}")
 
